1d90ac08ab83f42f170112bad9f101c25b3643f6
Bram Vandenbogaerde <bram.vandenbogaerde@gmail.com>
2018-02-01 17:38:11 +0100

    added ability to add events to the event wallet

--- cultuurcentrum.sol
+++ cultuurcentrum.sol
@@ -1,20 +1,30 @@
+pragma solidity ^0.4.16;
+
 contract Evenement {
-   string name;
-   uint available;
+   string mName;
+   uint mAvailable;
    
+
    // The tickets that are sold for this event
-   Ticket[] tickets;
+   Ticket[] mTickets;
 
    // The wallet that organises the event
-   EventWallet wallet;
+   EventWallet mWallet;
+
+   /**
+     * Primary constructor of the event
+     * This should be called from an EventWallet
+     */
+   function Evenement(string name, uint available, EventWallet w) public {
+      mName = name;
+      mAvailable = available;
+      mWallet = w;
+   }
 }
 
 contract Ticket {
    // The event for which the ticket was sold
    Evenement evenement;
-
-   // The wallet that sold the ticket
-   EventWallet wallet;
 
    // The owner of the ticket
    address owner;
@@ -30,21 +40,47 @@ contract Ticket {
 contract EventWallet{
    // A list of owners that own the wallet
    // An owner is allowed to change events, add them and delete them
-   address[] owners;
+   address[] mOwners;
 
    // The list of events that are managed by this wallet
-   Evenement[] evenement;
+   Evenement[] mEvenement;
 
    // The list of tickets that are sold by this wallet
-   Ticket[] tickets;
+   Ticket[] mTickets;
+
+   /**
+     * Primary constructor, used to initialize the internal data structures
+     */
+   function EventWallet() public {
+      // Add the creator of the wallet to the owner list
+      mOwners.push(msg.sender);
+   }
+
+   function isOwner(address p) public view returns(bool) {
+      // An akward way in solidity to check if a certain key exists in the mapping
+      // That is because there is not a function "contains" for mappings in Solidity
+      return mOwners[uint256(p)] > 0;
+   }
 
    /**
      * Only owner filter, scans the list of owners and checks that the sender is one of them
-     *
-     * @modifier
      */
    modifier only_owner() {
-      // TODO: implement
+      require(isOwner(msg.sender));
+      _;
+   }
+
+   /**
+     * Create and add an event to the wallet
+     * 
+     * @param name the name of the new event
+     * @param available_places the number of people that can attend the event
+     * @return the addres of the created event
+     */
+   function addEvent(string name, uint available_places) only_owner() public returns(address)  {
+      Evenement e = new Evenement(name, available_places, this);
+      mEvenement.push(e);
+      return e;
    }
 
 }
